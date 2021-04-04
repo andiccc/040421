436d6d0d460d2f465586ac11463f4ef65cedf0d9
andiccc <59291754+andiccc@users.noreply.github.com>
2021-04-04 23:02:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let Temp = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showString("" + (Temp))
+})
 basic.forever(function () {
-	
+    Temp = input.temperature()
 })
